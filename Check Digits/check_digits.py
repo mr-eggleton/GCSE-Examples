cb27e49c970b534afede82a82b688416c910184e
Martyn Eggleton <martyn.eggleton@gmail.com>
2022-11-07 22:21:10 +0000

    code forbinary shifts and check digits

---
+++ Check Digits/check_digits.py
@@ -0,0 +1,29 @@
+
+def getEANCheckDigit(digits):
+    sum = 0
+    for i in range(len(digits)):
+        num = int(digits[i])
+        if i % 2:
+            num *= 3
+        #print(digits[i],"num", num)
+        
+        sum += num
+    #print ("sum", sum)        
+    return str((10 - (sum % 10)) %10)
+
+def isEAN13Valid(barcode):
+    lastdigit = barcode[-1]
+    digits = barcode[:-1]
+    
+    checkdigit = getEANCheckDigit(digits)
+    
+    #print("lastdigit", lastdigit, "digits", digits, "checkdigit", checkdigit)
+    return lastdigit == checkdigit
+    
+    
+assert isEAN13Valid("4006381333931")
+
+assert isEAN13Valid("0123455432100")
+assert isEAN13Valid("9780345391803")
+assert isEAN13Valid("9780345391827")
+
